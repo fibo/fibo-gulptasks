6c5fed5ea83ab4c4983cc90fb341fe0b903a306e
fibo <casati_gianluca@yahoo.it>
2014-01-29 21:28:44 +0100

    added mkdir

--- README.md
+++ README.md
@@ -5,7 +5,19 @@ common gulp tasks among projects
 
 # Install
 
+Install packages
+
 ```bash
-npm install --save-dev github.com/fibo/fibo-gulptasks.git
+npm install -g gulp
+npm install --save-dev gulp gulp-util
+npm install --save-dev git://github.com/fibo/fibo-gulptasks.git
+```
+
+Then create a *gulpfile.js* like this
+
+```js
+var gulp = require('gulp')
+
+require('fibo-gulptasks')(gulp)
 ```
 
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
-var mocha = require('gulp-mocha')
+var mkdirp = require('mkdirp')
+  , mocha = require('gulp-mocha')
 
 module.exports = function (gulp) {
+
+  gulp.task('docs', function () {
+    mkdirp('docs/src/layouts')
+  })
 
   gulp.task('test', function () {
     gulp.src('test/*js')
         .pipe(mocha({reporter: 'nyan'}))
-  }
-
-  gulp.task('hello', function () {
-    console.log('hello)
   })
 
-  gulp.task('default', ['hello'])
+  gulp.task('default', ['test'])
 }
 
