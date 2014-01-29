77d94e418c479d9a9384731fe5fa453bafda1787
fibo <casati_gianluca@yahoo.it>
2014-01-29 21:45:22 +0100

    added jshintrc

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
+var base = './node_modules/fibo-gulpfiles/root'
+
 var mkdirp = require('mkdirp')
-  , mocha = require('gulp-mocha')
+  , mocha  = require('gulp-mocha')
 
 module.exports = function (gulp) {
 
-  gulp.task('docs', function () {
+  gulp.task('mkdirs', function () {
     mkdirp('docs/src/layouts')
+  })
+
+  gulp.task('.jshintrc', function () {
+    gulp.src('.jshintrc', {base: base})
+        .pipe(gulp.dest('.')
   })
 
   gulp.task('test', function () {
